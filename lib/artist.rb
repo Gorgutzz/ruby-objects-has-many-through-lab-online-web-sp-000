3c69daa69bcde6a2a57dc83e5d327f0a38e4daff
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-16 19:01:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,12 +6,15 @@ class Artist
   def initialize(name)
     @name = name
     @@all << self
-
   end
 
   def self.all
     @@all
   end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+
 
 
 
