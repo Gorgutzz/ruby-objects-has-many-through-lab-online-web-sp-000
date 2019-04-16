924943e0be6b4d6ddccb20960f0b9cef1f7c0cb1
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-16 19:36:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,28 +3,28 @@ class Artist
   @@all = []
 
   def initialize(name)
-    @name = name
+    @name = name 
     @@all << self
   end
 
   def self.all
-    @@all
+    @@all 
   end
 
   def new_song(name, genre)
     Song.new(name, self, genre)
   end
 
-  def songs
-    Song.all.select do |song|
+  def songs 
+     Song.all.select do |song|
       song.artist == self
     end
-  end
+  end 
 
   def genres
     songs.map do |song|
-      song.genre
+    song.genre
     end
   end
 
-end
+end 
