123a861989ed30c89c534a2e1f8550bb13b38603
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-16 19:46:53 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,17 @@
+class Appointment
+  attr_accessor :name, :doctor, :patient
+
+  @@all = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
